38417e1718e8c25c6ab903b23224b30202d64d27
Gabryel henrique sousa da silva <80103175+projetos-Gabryel@users.noreply.github.com>
2022-02-24 08:22:52 -0300

    Exemplo do PHP  com HTML e  CSSS

---
+++ aula_24_02_22/teste2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href= "css/estilo.css">
+    <title>Document</title>
+
+    
+</head>
+<body>
+    <h1> Teste pagina com HTML </h1>
+
+    <p><?php echo "<p class='oi'>olá Mundo!</p>";?>
+    </p>
+
+</body>
+</html>
