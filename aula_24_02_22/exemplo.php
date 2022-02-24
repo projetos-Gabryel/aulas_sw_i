d5398c6642a969a14ba7cb6ffbd4dd563a02c980
Gabryel henrique sousa da silva <80103175+projetos-Gabryel@users.noreply.github.com>
2022-02-24 08:57:42 -0300

    Add files via upload

---
+++ aula_24_02_22/exemplo.php
@@ -0,0 +1,18 @@
+<?php
+  $numero = 10;
+
+  $outro_numero = "10";
+
+
+  var_dump($numero);
+  echo "<br>";
+
+
+  if ($numero === $outro_numero){
+      echo "Valores iguais";
+
+  }
+  else{
+      echo "São valores diferentes";
+  }
+?>
