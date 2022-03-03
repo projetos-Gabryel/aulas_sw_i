f3b272c95797fee3672302aeab89ecdca120c1c8
Gabryel henrique sousa da silva <80103175+projetos-Gabryel@users.noreply.github.com>
2022-03-03 08:54:14 -0300

    Add files via upload

---
+++ aula_03_03_22/exemplo01.php
@@ -0,0 +1,8 @@
+<?php
+ echo "<h3>pòs-incremento</h3>";
+ $a = 5;
+ echo "deve ser 5: " . $a++ . "<br />\n";
+ echo "deve ser 6:"  . $a . "<br/>\n";
+
+
+?>
---
+++ aula_03_03_22/exemplo02.php
@@ -0,0 +1,21 @@
+<?php
+$soma = ($valor1 = 4 ) + 5;
+
+$valor = 20;
+$valor = 30;
+
+$soma += 1;
+$soma += $valor2;
+$soma *= $valor3;
+$soma %= 100;   
+
+
+$saudacao= "Bom";
+$saudacao= "Dia";
+
+echo $saudacao."Hoje vai ter" .soma. "% tristeza!";
+
+
+
+
+?>
