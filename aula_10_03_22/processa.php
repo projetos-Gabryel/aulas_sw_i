e11e8f4808baf654d80578dc168d9fc8fa802694
Gabryel henrique sousa da silva <80103175+projetos-Gabryel@users.noreply.github.com>
2022-03-10 09:10:53 -0300

    Add files via upload

---
+++ aula_10_03_22/processa.php
@@ -0,0 +1,14 @@
+ <?php
+//$num1 = 5;
+//$num2 = 10;
+
+
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+
+
+$soma = $num1 + $num2;
+
+echo "A soma de  ".$num1." + ".$num2."  é igual a " .$soma;
+
+?>
---
+++ exercio/exercicio.php
@@ -0,0 +1,11 @@
+<?php
+
+$txt=
+$num1 = 5;
+$num2 = 10;
+$num3 = 8;
+$soma = $num1 + $num2 + $num3;
+
+
+
+?>
